81020e83beee8adb12b0bd055f5022b7fb7cbf2c
AemanCh <144521071+amyxhehe@users.noreply.github.com>
2024-03-16 21:19:19 +0500

    Add files via upload

---
+++ 3)userInput.py
@@ -0,0 +1,8 @@
+#Module 1
+val = int(input("Enter a value: "))
+print(val)
+
+#Module 2
+name = input("Enter your name: ")
+country = input("Enter your country: ")
+print("{} is from {}.".format(name.capitalize(),country.capitalize()))
