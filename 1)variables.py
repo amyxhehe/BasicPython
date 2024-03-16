9e25cd8b49bad41f450a8ad690e90bd5cf63b6c8
AemanCh <144521071+amyxhehe@users.noreply.github.com>
2024-03-16 21:03:05 +0500

    Add files via upload

---
+++ 1)variables.py
@@ -0,0 +1,31 @@
+#Module 1
+print("Hello World")
+
+#Module 2
+sentence = "It's a beautiful day today."
+print(sentence)
+
+#Module 3
+phrase = "raining cats and dogs"
+num = 50
+print(phrase + str(num))
+
+#Module 4
+word = "pretty"
+number = 5
+for i in range(number):
+    print(word)
+
+#Module 5
+first_name = "Aeman"
+last_name = "Chaudhary"
+print(first_name + " " + last_name)
+
+#Module 6
+score1 = int(input("Enter score no 1: "))
+score2 = int(input("Enter score no 2: "))
+total = score1 + score2
+message = "Total score is %s "
+print(message % total)
+#messageString = "score1(%s) + score2(%s) = %s "
+#print(messageString % (score1, score2, total))
