fcc8499648e01e2022f3079af0b656c0d9907518
AemanCh <144521071+amyxhehe@users.noreply.github.com>
2024-03-21 15:06:39 +0500

    Add files via upload

---
+++ 4)ifElse/4.2)ifElse.py
@@ -0,0 +1,11 @@
+#Module 1
+cities = ["Islamabad","Karachi","Lahore","Quetta","Bahawalpur"]
+if "Lahore" in cities:
+    print(bool("Lahore"))
+
+#Module 2
+BOOL = True
+name = "Amy"
+if name != "Kat":
+    BOOL = False
+print(BOOL)
